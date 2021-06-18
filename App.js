520d89a5bf79a6c8e9d8785c84186d5d575882a9
Gustavo B. Justo <gustavojusto2006@gmail.com>
2021-06-17 21:09:44 -0300

    Trabalho de PAM2 finalizado, criamos uma aplicação de interface simples capaz de consumir, editar, cadastrar e excluir dados de uma API usando o Codeiginit

--- App.js
+++ App.js
@@ -1,21 +1,123 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import api from './components/api'
+ class App extends Component{
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    constructor(props){
+      super(props);
+      this.state = {
+        nome:'',
+        curso:'',
+        id:''
+      }
+      this.consultar = this.consultar.bind(this);
+      this.cadastrar = this.cadastrar.bind(this);
+      this.editar = this.editar.bind(this);
+      this.deletar = this.deletar.bind(this);
+    }
+
+    async consultar()
+    {
+       const response = await api.get('exemplo_api/pessoa/'+this.state.id);
+       this.setState({
+          nome: response.data.nome,
+          curso: response.data.curso,
+          id: response.data.id,
+       });
+    }
+
+    cadastrar()
+    {
+      let bodyFormData = new FormData();
+      bodyFormData.append('nome',`${this.state.nome}`);
+      bodyFormData.append('curso',`${this.state.curso}`);
+
+      api
+      ({
+        method:'post',
+        url:'exemplo_api/pessoa',
+        data: bodyFormData,
+        headers:{"Content-Type":"multipart/form-data"}
+      })
+    }
+
+    async editar()
+    {
+      const editar = await api.put(`exemplo_api/pessoa/${this.state.id}`,
+      {nome:`${this.state.nome}`, curso:`${this.state.curso}`});
+    }
+
+    deletar()
+    {
+      api.delete(`exemplo_api/pessoa/${this.state.id}`);
+    }
+
+    render(){
+      return (
+        <View style={styles.container}>
+          <Text style={styles.title}>CRUD API</Text>
+
+          <TextInput keyboardType="numeric" placeholder="ID" style={styles.inputs} 
+          onChangeText={(value)=>this.setState({id:value})}/>
+
+          <View style={styles.btn}>
+            <Button title="Consultar" onPress={this.consultar}/>
+          </View>
+
+          <TextInput placeholder="Nome..." style={styles.inputs} 
+          value={this.state.nome} onChangeText={(value)=>this.setState({nome:value})}/>
+
+          <TextInput placeholder="Curso..." style={styles.inputs}
+          value={this.state.curso} onChangeText={(value)=>this.setState({curso:value})}/>
+
+          <View style={styles.btn}>
+            <Button title="Cadastrar" style={styles.btn} onPress={this.cadastrar}/>
+          </View>
+
+          <View style={styles.btn}>
+            <Button title="Editar" style={styles.btn} onPress={this.editar}/>
+          </View>
+
+          <View style={styles.btn}>
+            <Button title="Excluir" style={styles.btn} onPress={this.deletar}/>
+          </View>  
+        </View>
+      );
+    }
+ }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#b1c5fc',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: 
+  {
+    fontSize: 30,
+    borderBottomWidth: 3,
+    borderBottomColor: 'grey',
+    
+  },
+
+  inputs: 
+  {
+    marginTop: 20,
+    borderWidth: 2,
+    borderColor: 'grey',
+    width: 175,
+    textAlign: 'center',
+    backgroundColor: '#fff',
+  },
+  btn: 
+  {
+    marginTop: 10,
+    width: 150,
+    backgroundColor: "black",
+  },
 });
+
+export default App;
---
+++ components/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create
+({
+        baseURL:'https://jussimarleal.com.br/'
+});
+
+export default api;
+
